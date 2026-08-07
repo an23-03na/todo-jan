6061bd2ee577d804c3e77a3d74fb84b41e95ed1b
an23-03na <annachyan2303@gmail.com>
2026-08-07 21:32:21 +0400

    update

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -11,6 +11,11 @@ export const auth = betterAuth({
   trustedOrigins: [process.env.FRONTEND_URL!],
   advanced: {
     cookiePrefix: "auth",
+    crossSubDomainCookies: {
+      enabled: true,
+      domain: ".mk-flower.am",
+    },
+    useSecureCookies: true,
   },
   emailAndPassword: {
     enabled: true,
